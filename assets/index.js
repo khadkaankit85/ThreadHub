0c37716b4e41dd027466231a0f83de087cbaa5a0
Angkit Khadka <khadkaankit85@gmail.com>
2024-07-19 18:23:21 -0400

    failing to add svg

---
+++ assets/index.js
@@ -0,0 +1,32 @@
+import favicon from './images/favicon.png';
+import icon from './images/icon.png';
+import adaptiveIcon from './images/adaptive-icon.png';
+import angryFaceWithDarkBG from "./images/icons8-angry-face-meme-dark.svg"
+import angryFaceWithLightBG from "./images/icons8-angry-face-meme.svg"
+import backButtonWithDarkBG from "./images/icons8-back-dark.svg"
+import backButtonWithLightBG from "./images/icons8-back-dark.svg"
+import FavouriteIcon from "./images/icons8-favorite-folder.svg"
+import unlikedIcon from "./images/icons8-heart-dark.svg"
+import likedIcon from "./images/icons8-heart-filled-dark.svg"
+import notBadMeme from "./images/icons8-not-bad-meme-dark.svg"
+import scaredFaceMeme from "./images/icons8-scared-face-meme-dark.svg"
+import shrugIcon from "./images/icons8-shrug-emoticon-dark.svg"
+import wink from "./images/icons8-wink.svg"
+
+export {
+    favicon,
+    icon,
+    adaptiveIcon,
+    angryFaceWithDarkBG,
+    angryFaceWithLightBG,
+    backButtonWithDarkBG,
+    backButtonWithLightBG,
+    FavouriteIcon,
+    unlikedIcon,
+    likedIcon,
+    notBadMeme,
+    scaredFaceMeme,
+    shrugIcon,
+    wink
+}
+export default { FavouriteIcon }
